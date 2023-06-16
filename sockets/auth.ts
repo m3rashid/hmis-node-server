ebb896dd1c77c54d675783cf139207bc76f4b883
m3rashid <m3rashid.hussain@gmail.com>
2023-06-16 22:01:07 +0530

    fix(app): sockets

---
+++ sockets/auth.ts
@@ -0,0 +1,18 @@
+import JWT from 'jsonwebtoken';
+import type { Socket } from 'socket.io';
+
+const checkSocketAuth = (socket: Socket, next: any) => {
+  try {
+    const token = socket.handshake.auth.token;
+    if (!token) throw new Error('Auth error');
+    const payload = JWT.verify(token, process.env.ACCESS_SECRET);
+
+    socket.data.user = payload.sub;
+    return next();
+  } catch (err) {
+    console.log('Socket Err', err);
+    next(err);
+  }
+};
+
+export default checkSocketAuth;
