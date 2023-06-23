16698af86684ab2d5a3cdc3e81e735ed9a132ca5
m3rashid <m3rashid.hussain@gmail.com>
2023-06-23 17:14:39 +0530

    feat(auth): setup

--- controllers/profile.ts
+++ controllers/profile.ts
@@ -1,17 +1,15 @@
-import type { Request, Response } from 'express'
-import { ProfileModel } from '../models/profile'
+import type { Request, Response } from 'express';
+import { ProfileModel } from '../models/profile';
 
 export const createProfile = async (req: Request, res: Response) => {
-	const newProfile = new ProfileModel(req.body)
-	const profile = await newProfile.save()
-	res.status(200).json(profile)
-}
+  const newProfile = new ProfileModel(req.body);
+  const profile = await newProfile.save();
+  res.status(200).json(profile);
+};
 
 export const updateProfile = async (req: Request, res: Response) => {
-	const profile = await ProfileModel.findByIdAndUpdate(
-		req.body._id,
-		req.body,
-		{ new: true }
-	)
-	res.status(200).json(profile)
-}
+  const profile = await ProfileModel.findByIdAndUpdate(req.body._id, req.body, {
+    new: true,
+  });
+  res.status(200).json(profile);
+};
