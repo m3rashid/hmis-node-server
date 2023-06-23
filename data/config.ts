d1a025e8ea8837c3204663a2d2a11647d1be93f0
m3rashid <m3rashid.hussain@gmail.com>
2023-06-23 07:25:19 +0530

    feat(config): setup

--- data/config.ts
+++ data/config.ts
@@ -1,18 +1,19 @@
 export const appConfig = {
-	app: {
-		name: 'HMIS',
-		version: '1.0.0',
-		fullName: 'Health Management and Informatics System'
-	},
-	appColors: {
-		primary: '#00BDC1',
-		primaryHover: '#E6FFFB',
-		secondary: '#484C56'
-	},
-	colors: {
-		success: '#22C55E',
-		warning: '#F59E0B',
-		danger: '#F43F5E',
-		info: '#0EA5E9'
-	}
-} as const
+  app: {
+    name: 'HMIS',
+    version: '1.0.0',
+    fullName: 'Health Management and Informatics System',
+    theme: 'light',
+  },
+  appColors: {
+    primary: '#6366f1',
+    primaryHoverLight: '#818cf8',
+    primaryHoverDark: '#312e81',
+  },
+  colors: {
+    success: '#22C55E',
+    warning: '#F59E0B',
+    danger: '#F43F5E',
+    info: '#0EA5E9',
+  },
+} as const;
